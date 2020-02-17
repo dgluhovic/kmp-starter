7ad67c64c39b5ad195d89a18cee78110e83165da
Djordje Gluhovic <dj@instacart.com>
2020-02-17 13:54:21 -0800

    cleanup

--- setup-mac.sh
+++ setup-mac.sh
@@ -9,7 +9,3 @@ createdb $DB_NAME
 psql -d $DB_NAME -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
 psql -d $DB_NAME -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_USER;"
 psql -d $DB_NAME -c "CREATE EXTENSION POSTGIS;"
-
-# publish to local mvn repo
-# https://discuss.kotlinlang.org/t/creating-multiplatform-library-for-usage-in-other-multiplatform-projects/12860
-./gradlew data:build
